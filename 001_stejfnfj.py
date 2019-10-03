d91f9d9fe1987dd9d807af3ec5f7896513068fb8
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-10-03 09:48:09 -0700

    Add files via upload

---
+++ 001_stejfnfj.py
@@ -0,0 +1,13 @@
+# script mode / batch mode
+'''
+====>> create a new file
+====>> write all python statements
+===>> save the file as .py extension
+===>>  directly we can run to get the output
+'''
+a = 10
+b =20
+print a+b
+print a - b
+print a*b
+print a/b
